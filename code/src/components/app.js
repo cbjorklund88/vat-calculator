6ef501f74b09f8b093f9297773d7f31817362632
cbjorklund88 <clara.bjorklund@hotmail.com>
2018-10-19 14:33:41 +0200

    added some styling

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -8,24 +8,25 @@ class App extends React.Component {
     this.state = {
       vatRate: 25,
       incVat: 0,
-      exVat: 0,
-      vatSum: 0
+      exVat: 0
     }
   }
 
 handleInput = (e) => {
+  const vat = parseInt(e.target.value)
+  const { incVat } = this.state
+  const exVat = incVatToExtVat(vat, incVat)
+
   this.setState({
-    vatRate: parseInt(e.target.value),
-    incVat: 0,
-    exVat: 0
+    vatRate: vat,
+    exVat
   })
 }
 
 handleInputIncVat = (e) => {
   this.setState({
     exVat: incVatToExtVat(this.state.vatRate, parseInt(e.target.value)),
-    incVat: parseInt(e.target.value),
-    vatSum: parseInt(e.target.value)
+    incVat: parseInt(e.target.value)
   })
 }
 
@@ -33,66 +34,69 @@ handleInputExVat = (e) => {
   this.setState({
     incVat: exVatToIncVat(this.state.vatRate, parseInt(e.target.value)),
     exVat: parseInt(e.target.value),
-    vatSum: parseInt(e.target.value)
   })
 }
 
 render() {
   return (
-    <div>
+    <div className="wrapper">
       <h1>Momsuträknare</h1>
       <form>
-        <div>
-          <label htmlFor="25percent">25%</label>
-          <input
-            onChange={this.handleInput}
-            id="25percent"
-            type="radio"
-            value="25"
-            checked={this.state.vatRate === 25} />
-        </div>
+        <div className="percentege-container">
+          <div>
+            <label htmlFor="25percent">25%</label>
+            <input
+              onChange={this.handleInput}
+              id="25percent"
+              type="radio"
+              value="25"
+              checked={this.state.vatRate === 25} />
+          </div>
 
-        <div>
-          <label htmlFor="12percent">12%</label>
-          <input
-            onChange={this.handleInput}
-            id="12percent"
-            type="radio"
-            value="12"
-            checked={this.state.vatRate === 12} />
-        </div>
+          <div>
+            <label htmlFor="12percent">12%</label>
+            <input
+              onChange={this.handleInput}
+              id="12percent"
+              type="radio"
+              value="12"
+              checked={this.state.vatRate === 12} />
+          </div>
 
-        <div>
-          <label htmlFor="6percent">6%</label>
-          <input
-            onChange={this.handleInput}
-            id="6percent"
-            type="radio"
-            value="6"
-            checked={this.state.vatRate === 6} />
+          <div>
+            <label htmlFor="6percent">6%</label>
+            <input
+              onChange={this.handleInput}
+              id="6percent"
+              type="radio"
+              value="6"
+              checked={this.state.vatRate === 6} />
+          </div>
         </div>
-        <div>
+        <div className="inputs-container">
+          <div className="inc-vat-container">
         Inklusive moms (kr):
-          <input
-            type="number"
-            name="incVat"
-            onChange={this.handleInputIncVat}
-            value={this.state.incVat} />
-        </div>
-        <div>
+            <input
+              type="number"
+              name="incVat"
+              onChange={this.handleInputIncVat}
+              value={this.state.incVat} />
+          </div>
+          <div className="ex-vat-container">
         Exclusive moms (kr):
-          <input
-            type="number"
-            name="exVat"
-            onChange={this.handleInputExVat}
-            value={(this.state.exVat).toFixed(2)} />
-        </div>
-        <div>
-        Momssumma (kr):
-          <input
-            type="number"
-            name="vatSum"
-            value={(this.state.incVat - this.state.exVat).toFixed(2)} />
+            <input
+              type="number"
+              name="exVat"
+              onChange={this.handleInputExVat}
+              value={(this.state.exVat).toFixed(2)} />
+          </div>
+          <div className="sum-vat-container">
+          Momssumma (kr):
+            <input
+              type="number"
+              name="vatSum"
+              value={(this.state.incVat - this.state.exVat).toFixed(2)} />
+          </div>
         </div>
       </form>
     </div>
